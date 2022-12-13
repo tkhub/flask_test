b0083e3ef25994bd965f7c1ba7caa426eb4d5ad9
tkhub <takap.colonel@gmail.com>
2022-12-14 01:16:19 +0900

    template OK

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+Ref : https://flask.palletsprojects.com/en/2.0.x/quickstart/
+Ref : https://jinja.palletsprojects.com/en/3.1.x/templates/
 Learing in : https://www.youtube.com/watch?v=bzbrpkbjWe8&t=2140s
+Learing in : https://www.youtube.com/watch?v=mW0_60SRr3s&t=239s
--- sample.py
+++ sample.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import render_template
 
+Black_Tri_Stars = [ 'ガイア', 'オルテガ', 'マッシュ']
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Flask!</p>"
+    return render_template("hello.html", items = Black_Tri_Stars)
